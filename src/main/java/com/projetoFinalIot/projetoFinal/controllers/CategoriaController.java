72e0cc9c9b8bc96a3a3c3bceca669c1049b12a82
luizGonzaga29 <luizgonzagacabral29@gmail.comgit config --global user.email luizgonzagacabral29@gmail.com>
2022-06-06 18:28:47 -0300

    buscar Produtos da mesma categoria

--- src/main/java/com/projetoFinalIot/projetoFinal/controllers/CategoriaController.java
+++ src/main/java/com/projetoFinalIot/projetoFinal/controllers/CategoriaController.java
@@ -1,13 +1,23 @@
 package com.projetoFinalIot.projetoFinal.controllers;
 
-import com.projetoFinalIot.projetoFinal.entidades.Categoria;
-import com.projetoFinalIot.projetoFinal.services.CategoriaService;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-import java.util.List;
+import com.projetoFinalIot.projetoFinal.entidades.Categoria;
+import com.projetoFinalIot.projetoFinal.entidades.Produto;
+import com.projetoFinalIot.projetoFinal.services.CategoriaService;
 
 @CrossOrigin("*")
 @RestController
@@ -43,5 +53,11 @@ public class CategoriaController {
     	cat.setId(id);
     	categoriaService.update(cat);
     	return ResponseEntity.noContent().build();
+    }
+    
+    @GetMapping(value = "/{id}/produto")
+    public ResponseEntity<List<Produto>> findProdutos(@PathVariable Integer id){
+    	Categoria cat = categoriaService.findById(id);
+        return ResponseEntity.status(HttpStatus.OK).body(cat.getProdutos());
     }
 }
